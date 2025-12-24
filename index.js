6cfa1a6c179044d4ae64a167a389c7f9d1fce9e4
triloon <dimontopcik59@gmail.com>
2025-12-24 20:19:45 +0200

    HW-17

---
+++ index.js
@@ -0,0 +1,17 @@
+let title = document.querySelector('#title');
+let btn = document.querySelector('#button');
+btn.textContent = title.textContent;
+
+
+let img = document.querySelector('#img');
+img.src = 'https://picsum.photos/200/300'
+
+
+let link = document.querySelector('#link');
+link.href = 'https://www.firefox.com/uk/';
+let image = document.querySelector('#image');
+image.alt = 'rewasdd';
+
+
+let li = document.querySelector('li');
+li.textContent = 'Changed text';
